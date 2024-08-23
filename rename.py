813c3a7c300faee56e68a74b939b21239e62facf
Szymon Sieradzki <s.sieradzki@sellintegrosellintegro.onmicrosoft.com>
2024-08-23 23:35:49 +0200

    rename

--- rename.py
+++ rename.py
@@ -1,23 +1,23 @@
 import os
 
 # Set the directory where the files are located
-directory = './results_new/atpe7'
+directory = './results_new/atpe9'
 
 # Traverse through all files in the directory
 for filename in os.listdir(directory):
     # Check if the file starts with 'atpe4'
-    if filename.startswith('atpe4'):
+    if filename.startswith('atpe8'):
         # Construct the new file name by replacing 'atpe4' with 'atpe5'
-        new_filename = 'atpe7' + filename[5:]
+        new_filename = 'atpe9' + filename[5:]
         # Rename the file
         os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
         print(f'Renamed: {filename} -> {new_filename}')
 
 for filename in os.listdir(directory):
     # Check if the file ends with 'ATPE4'
-    if filename.endswith('ATPE4.csv'):
+    if filename.endswith('ATPE8.csv'):
         # Construct the new file name by replacing 'ATPE4' with 'ATPE5'
-        new_filename = filename[:-9] + 'ATPE7.csv'
+        new_filename = filename[:-9] + 'ATPE9.csv'
         # Rename the file
         os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
         print(f'Renamed: {filename} -> {new_filename}')
